fb6037e19fa8606747a413177ede336a0a506dc6
Carl J <carlj1881@yahoo.com>
2020-10-09 05:40:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,15 +15,15 @@ class Triangle
     all_zero = a <= 0 || b <= 0 || c <= 0
     less_than_1 = a + b < c || a + c < b || b + c < a
     not_equilateral = a < b && b > c
-    all_greater = a > b && b > c
-    all_lesser = a < b && b < c
+    # all_greater = a > b && b > c
+    # all_lesser = a < b && b < c
     if all_zero || less_than_1 || not_equilateral || all_greater || all_lesser
       raise TriangleError
     elsif a == b && a == c && a != 0 && b != 0 && c != 0
       return :equilateral
     elsif a == b || a == c || b == c
       return :isosceles
-    else
+    elsif 
       return :scalene
     end
 
