921ee9481ab22db45d1651503e4dc02da8a33f82
Carl J <carlj1881@yahoo.com>
2020-10-09 06:33:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
   def kind
 
     tri_inequality = a + b > c || b + c > a || a + c > b
-    all_zero = a <= 0 || b <= 0 || c <= 0 && !
+    all_zero = a <= 0 || b <= 0 || c <= 0
     if tri_inequality && all_zero && a == b && a == c && a != 0 && b != 0 && c != 0
       :equilateral
     elsif tri_inequality && all_zero && a == b || a == c || b == c
