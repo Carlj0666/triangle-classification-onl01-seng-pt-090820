51cd97bf50b5c787166e8a6ab3ceb785a5e189fe
Carl J <carlj1881@yahoo.com>
2020-10-09 02:49:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,14 @@
 class Triangle
   
-  
+  attr_accessor :tri_length_a, :tri_length_b, :tri_length_a
   
   def initialize(tri_length_a:, tri_length_b:,tri_length_c:)
+    @tri_length_a = tri_length_a
+    @tri_length_b = tri_length_b
+    @tri_length_c = tri_length_c
+  end
+  
+  def kind
     
     
   end
