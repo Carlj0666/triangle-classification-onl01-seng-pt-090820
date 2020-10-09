5648d06b86e20f940d8757d2acfd77ade4792b73
Carl J <carlj1881@yahoo.com>
2020-10-09 03:09:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
   end
   
   def kind
-    if tri_length_a = tri_length_b && tri_length_a == tri_length_c
+    if tri_length_a == tri_length_b && tri_length_a == tri_length_c
       return :equilateral
-      elsif tri_length_a
+      elsif tri_length_a == tri_length_a && 
     
   end
   
