99d0e3ac37202b719822231b0d37b80352d8a631
Carl J <carlj1881@yahoo.com>
2020-10-09 06:20:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,20 +12,23 @@ class Triangle
 
 
   def kind
-    all_zero = a <= 0 || b <= 0 || c <= 0
-    less_than_1 = a + b < c || a + c < b || b + c < a
-    not_equilateral = a < b && b > c
-    all_greater = a > b && b > c
-    all_lesser = a < b && b < c
-    if all_zero || less_than_1 || not_equilateral || all_greater || all_lesser
-      raise TriangleError
-    elsif a == b && a == c && a != 0 && b != 0 && c != 0
+    
+    if a == b && a == c && a != 0 && b != 0 && c != 0
       :equilateral
-    elsif a == b || a == c || b == c
-      :isosceles
-    else
-      :scalene
-    end
+    # all_zero = a <= 0 || b <= 0 || c <= 0
+    # less_than_1 = a + b < c || a + c < b || b + c < a
+    # not_equilateral = a < b && b > c
+    # all_greater = a > b && b > c
+    # all_lesser = a < b && b < c
+    # if all_zero || less_than_1 || not_equilateral || all_greater || all_lesser
+    #   raise TriangleError
+    # elsif a == b && a == c && a != 0 && b != 0 && c != 0
+    #   :equilateral
+    # elsif a == b || a == c || b == c
+    #   :isosceles
+    # else
+    #   :scalene
+    # end
   end
   
   
