a4b0640c14b9c73d3d0e19a1cd6fbd40a72ac07a
Carl J <carlj1881@yahoo.com>
2020-10-09 05:15:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,9 @@ class Triangle
   
     
   def kind
-
+    if a == 0 || b == 0 || c == 0
+      puts error.message
+    end
     if a == b && a == c
       return :equilateral
     elsif a == b || a == c || b == c
