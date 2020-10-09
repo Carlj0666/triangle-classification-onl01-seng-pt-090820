7629dd5cb3f9b6a703c6b5df50a425b234ed3855
Carl J <carlj1881@yahoo.com>
2020-10-09 04:36:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,17 +1,19 @@
 class Triangle
   
-  attr_accessor :tri_length_a, :tri_length_b, :tri_length_a
+  attr_accessor :a, :b, :c
   
-  def initialize(tri_length_a:, tri_length_b:,tri_length_c:)
-    @tri_length_a = tri_length_a
-    @tri_length_b = tri_length_b
-    @tri_length_c = tri_length_c
+  def initialize(a:, b:, c:)
+    @a = a
+    @b = b
+    @c = c
   end
   
   def kind
-    if tri_length_a == tri_length_b && tri_length_a == tri_length_c
+    if a == 0 || b == 0 || c == 0
+      message 
+    if a == b && a == c
       return :equilateral
-      elsif tri_length_a != tri_length_b && tri_length_c || tri_length_b != tri_length_a && tri_length_c || tri_length_c != tri_length_a && tri_length_b
+      elsif a != b && a != c || b != tri_length_a && tri_length_c || tri_length_c != tri_length_a && tri_length_b
       return :isosceles
     else
       return :scalene
