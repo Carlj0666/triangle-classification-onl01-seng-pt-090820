ebebe38355ac93899a5d0c7916d52d010e530a0b
Carl J <carlj1881@yahoo.com>
2020-10-09 04:32:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,14 @@ class Triangle
   end
   
   def kind
-    # if tri_length_a == tri_length_b && tri_length_a == tri_length_c
-    #   return :equilateral
-    #   elsif tri_length_a != tri_length_b && tri_length_c || tri_length_b != tri_length_a && tri_length_c || tri_length_c != tri_length_a && tri_length_b
-    #   return :isosceles
-    # else
+    if tri_length_a == tri_length_b && tri_length_a == tri_length_c
+      return :equilateral
+      elsif tri_length_a != tri_length_b && tri_length_c || tri_length_b != tri_length_a && tri_length_c || tri_length_c != tri_length_a && tri_length_b
+      return :isosceles
+    else
       return :scalene
     end
-  # end
+   end
   
   class TriangleError < StandardError
     def message
