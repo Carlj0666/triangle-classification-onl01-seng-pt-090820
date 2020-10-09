68275aacaf4a535cff853bfac1f04658339e845c
Carl J <carlj1881@yahoo.com>
2020-10-09 05:36:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,13 +12,13 @@ class Triangle
 
     
   def kind
-    all_zero = a != 0 || b != 0 || c != 0
+    all_zero = a <= 0 || b <= 0 || c <= 0
     less_than_1 = a + b < c || a + c < b || b + c < a
     not_equilateral = a < b && b > c
     all_greater = a > b && b > c
     if all_zero || less_than_1 || not_equilateral || all_greate
       raise TriangleError
-    elsif a == b && a == c
+    elsif a == b && a == c && a != 0 && b != 0 && c != 0
       return :equilateral
     elsif a == b || a == c || b == c
       return :isosceles
