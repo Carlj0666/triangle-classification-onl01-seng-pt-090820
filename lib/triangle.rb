4f74fa09194c46daf070707e2c920732241a44a0
Carl J <carlj1881@yahoo.com>
2020-10-09 06:16:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,27 +12,23 @@ class Triangle
 
     
   def kind
-    if a == b && a == c && c == b && a != 0 || b != 0 || c != 0
-        return :equilateral
-      elsif a == b || a == c || b == c
-        return :isosceles
-    end
+
+    all_zero = a <= 0 || b <= 0 || c <= 0
+    less_than_1 = a + b < c || a + c < b || b + c < a
+    not_equilateral = a < b && b > c
+    scalene = a > b && b > c && a != c
+    all_lesser = a < b && b < c
+    if all_zero || less_than_1 || not_equilateral || all_lesser
+      raise TriangleError
+    elsif a == b && a == c && c == b && a != 0 || b != 0 || c != 0
+      return :equilateral
+    elsif a == b || a == c || b == c
+      return :isosceles
+    elsif scalene
+      return :scalene
     end
 
-    # all_zero = a <= 0 || b <= 0 || c <= 0
-    # less_than_1 = a + b < c || a + c < b || b + c < a
-    # not_equilateral = a < b && b > c
-    # scalene = a > b && b > c && a != c
-    # all_lesser = a < b && b < c
-    # if all_zero || less_than_1 || not_equilateral || all_lesser
-    #   raise TriangleError
-    # elsif a == b && a == c && c == b && a != 0 || b != 0 || c != 0
-    #   return :equilateral
-    # elsif a == b || a == c || b == c
-    #   return :isosceles
-    # elsif scalene
-    #   return :scalene
-    # end
+  end
   
   
   class TriangleError < StandardError
@@ -42,3 +38,10 @@ class Triangle
   end
   
 end
+
+
+  # a, b, c = [a, b, c].sort
+  # raise TriangleError if a <= 0 or a + b <= c
+  # return :equilateral if a == c
+  # return :isosceles if a == b or b == c
+  # return :scalene
